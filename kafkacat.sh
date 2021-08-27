fdde6aa92372833ffe6455d09abf9f8cfc7db1e9
Jacob Lindgren <jlindgre@redhat.com>
2021-08-27 11:52:01 -0500

    default to event-stream, but allow a param

--- kafkacat.sh
+++ kafkacat.sh
@@ -2,7 +2,7 @@
 set -x
 
 BROKER=$(clowdRun env | grep KAFKA_BROKERS | cut -d= -f2)
-TOPIC=$(clowdRun env | grep TOPIC_NAME | grep event-stream | cut -d= -f2)
+TOPIC=$(clowdRun env | grep TOPIC_NAME | grep ${1:-event-stream} | cut -d= -f2)
 
 kafkacat -b $BROKER -t $TOPIC -o beginning -C \
   -f '\nKey (%K bytes): %k
